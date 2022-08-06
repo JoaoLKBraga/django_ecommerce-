459cef144dfd10409e7a29b64d4232aa87287865
João Lucas <joaolkbraga01@gmail.com>
2022-08-05 22:11:48 -0300

    create about & contact

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -3,5 +3,22 @@ from django.shortcuts import render
 
 
 def home_page(request):
-  contexto = {}
-  return render(request, "home_page.html", contexto)
+  context = {
+    "title": "Página principal",
+    "content": "Bem-vindo a página principal"
+  }
+  return render(request, "home_page.html", context)
+
+def about_page(request):
+  context = {
+    "title": "Página sobre",
+    "content": "Bem-vindo a página sobre"
+  }
+  return render(request, "about/view.html", context)
+
+def contact_page(request):
+  context = {
+    "title": "Página de contato",
+    "content": "Bem-vindo a página de contato"
+  }
+  return render(request, "contact/view.html", context)
