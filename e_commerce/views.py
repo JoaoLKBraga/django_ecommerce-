8cdd2a8bde223620fbbf688e8fe6324cbe033141
João Lucas <joaolkbraga01@gmail.com>
2022-08-09 17:54:46 -0300

    create contact form

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def home_page(request):
@@ -17,8 +18,12 @@ def about_page(request):
   return render(request, "about/view.html", context)
 
 def contact_page(request):
+  contact_form = ContactForm(request.POST or None)
   context = {
     "title": "Página de contato",
-    "content": "Bem-vindo a página de contato"
+    "content": "Bem-vindo a página de contato",
+    "form": contact_form
   }
+  if contact_form.is_valid():
+    print(contact_form.cleaned_data)
   return render(request, "contact/view.html", context)
